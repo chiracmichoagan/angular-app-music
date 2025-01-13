e54da2a3395b4624c100edcef96130b8ecd98bea
Chirac Michoagan <michoaganchiracdona@gmail.com>
2025-01-13 12:42:10 +0100

    first commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
   messageError?: string = undefined;
 
